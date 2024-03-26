38121bedd626636f34acc6ba2aabbfb6d3355a5e
AntalNorbert23 <anorbertcsaba@yahoo.com>
2024-03-26 21:36:10 +0200

    added weekly weather and localstorage set

--- weather_project/src/components/AsyncCityview.vue
+++ weather_project/src/components/AsyncCityview.vue
@@ -9,7 +9,7 @@
         </div>
         <div class="flex flex-col items-center text-white py-12">
             <h1 class="text-4xl mb-2">{{ route.params.city }}</h1>
-            <p class="text-sm mb12">
+            <p class="text-sm mb-12">
                 {{  new Date(weatherData.currentTime).toLocaleDateString(
                         "en-us",
                         {
@@ -27,6 +27,82 @@
                     )    
                 }}
             </p>
+            <p class="text-8xl mb-8">
+                {{ ((Math.round(weatherData.current.temp)-32)*5/9).toFixed(1) }}°C
+            </p>
+                <p>
+                    Feels like
+                    {{ ((Math.round(weatherData.current.feels_like)-32)*5/9).toFixed(1) }}°C
+                </p>
+                <p class="capitalize">
+                    {{ weatherData.current.weather[0].description }}
+                </p>
+                <img class="w-[150px] h-auto"
+                     :src="`http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`"
+                      alt="weather icon"
+                >
+        </div>
+
+        <hr class="border-white border-opacity-10 border w-full">
+
+        <div class="max-w-screen-md w-full py-12">
+                <div class="mx-8 text-white">
+                    <h2 class="mb-4">Hourly weather</h2>   
+                </div>
+                <div class="flex gap-10 overflow-x-scroll">
+                    <div v-for="hourData in weatherData.hourly" 
+                         :key="hourData.dt"
+                         class="flex flex-col gap-4 items-center"
+                    >
+                        <p class="whitespace-nowrap text-md">
+                            {{ new Date(hourData.currentTime).toLocaleTimeString(
+                                'en-us',{
+                                    hour:'numeric',
+                                })
+                             }}
+                        </p>
+                        <img class="w-auto h-[50px] object-cover"
+                             :src="`http://openweathermap.org/img/wn/${hourData.weather[0].icon}@2x.png`"
+                              alt="hourly weather"
+                        >
+                        <p class="text-xl">
+                            {{ ((Math.round(hourData.temp)-32)*5/9).toFixed(1) }}°C
+                        </p>
+                    </div>
+                </div>
+        </div>
+
+        <hr class="border-white border-opacity-10 border w-full">
+        <div class="max-w-screen-md w-full py-12">
+            <div class="mx-8 text-white">
+                <h2 class="mb-4">7 day forecast</h2>
+                <div class="flex items-center"
+                     v-for="day in weatherData.daily"
+                     :key="day.dt"
+                >
+                     
+                      <p class="flex-1">
+                            {{ 
+                                new Date(day.dt*1000).toLocaleDateString(
+                                    "en-us",
+                                    {
+                                       weekday:"long", 
+                                    }
+                                )
+
+                            }}
+                      </p>
+                      <img class="w-[50px] h-[50px] object-cover"
+                           :src="`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`"
+                           alt="weather icon"
+                      >
+                      <div class="flex gap-2 flex-1 justify-end">
+                            <p>H: {{ ((Math.round(day.temp.max)-32)*5/9).toFixed(1) }}°C</p>
+                            <p>L: {{ ((Math.round(day.temp.min)-32)*5/9).toFixed(1) }}°C</p>
+                      </div>
+
+                </div>
+            </div>
         </div>
     </div>
 </template>
--- weather_project/src/components/SiteNavigation.vue
+++ weather_project/src/components/SiteNavigation.vue
@@ -12,7 +12,10 @@
                 <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"
                    @click="toggleModal"
                 ></i>
-                <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
+                <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"
+                   @click="addCity"
+                   v-if="route.query.preview"
+                ></i>
             </div>
 
             <BaseModal :modalActive="modalActive" @close-modal="toggleModal">
@@ -38,12 +41,38 @@
 
 <script setup>
     import { ref } from 'vue';
-    import { RouterLink} from "vue-router";
+    import { RouterLink,useRoute,useRouter } from "vue-router";
     import BaseModal from './BaseModal.vue';
+    import { uid } from "uid";
 
+    const savedCities=ref([]);
+    const route=useRoute();
+    const router=useRouter()
+    const addCity=()=>{
+        if(localStorage.getItem('savedCities')){
+            savedCities.value=JSON.parse(localStorage.getItem("savedCities"))
+        }
+
+        const locationObj={
+            id:uid(),
+            state:route.params.state,
+            city:route.params.city,
+            coords:{
+                lat:route.query.lat,
+                lng:route.query.lng,
+            }
+        }
+
+        savedCities.value.push(locationObj);
+        localStorage.setItem('savedCities', JSON.stringify(savedCities.value));
+
+        let query=Object.assign({},route.query);
+        delete query.preview;
+        router.replace({ query})
+    }
 
     const modalActive=ref(null);
     const toggleModal=()=>{
-        modalActive.value=!modalActive.value
+        modalActive.value=!modalActive.value;
     }
 </script>

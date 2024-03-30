efc3f9f3a61b1e05c5eae0094997cc5cdfb5516b
AntalNorbert23 <anorbertcsaba@yahoo.com>
2024-03-30 21:12:49 +0200

    completed project

--- weather_project/src/App.vue
+++ weather_project/src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
       <SiteNavigation/>
-      <RouterView/>
+      <RouterView v-slot="{ Component }">
+          <Transition name="page" mode="out-in">
+            <component :is="Component"/>
+          </Transition>
+      </RouterView>
   </div>
 </template>
 
@@ -12,5 +16,11 @@
 </script>
 
 <style scoped>
+  .page-enter-active,.page-leave-active {
+  transition: 600ms ease all;
+}
 
+.page-enter-from,.page-leave-to{
+  opacity: 0;
+}
 </style>
